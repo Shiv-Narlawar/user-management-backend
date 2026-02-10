6a8d5ebb02df70a19839cf0d4326d665f1fb9178
Shiv-Narlawar <shivam@ngenux.com>
2026-02-09 22:39:45 -0800

    Added .gitignore and .env file

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
+import "dotenv/config";
 import app from "./app";
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
